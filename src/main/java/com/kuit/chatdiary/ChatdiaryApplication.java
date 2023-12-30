9741b3a4fb1d656e6d0709a0af9d8a275f621c52
nonaninona <starcraft0529@gmail.com>
2023-12-30 18:55:30 +0900

    initial spring project

---
+++ src/main/java/com/kuit/chatdiary/ChatdiaryApplication.java
@@ -0,0 +1,13 @@
+package com.kuit.chatdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatdiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatdiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kuit/chatdiary/ChatdiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuit.chatdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatdiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
