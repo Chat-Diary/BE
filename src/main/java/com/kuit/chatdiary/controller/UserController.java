4c0e80b460164982b60f312788f08636d76bb17f
Mouon <ahemsapsldk@gmail.com>
2024-01-04 10:09:56 +0900

    Feat: api 연동 테스트 추가
    api 연동 페이지와 컨트롤러 추가

--- src/main/java/com/kuit/chatdiary/controller/UserController.java
+++ src/main/java/com/kuit/chatdiary/controller/UserController.java
@@ -1,10 +1,15 @@
 package com.kuit.chatdiary.controller;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class UserController {
 
+    @GetMapping("/chat")
+    public String chat() {
+        return "redirect:/chat.html";
+    }
     @GetMapping("/user")
     public String user() {
         return "user";
