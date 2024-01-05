5197a97e1d1d0099ef45139063ec45d69c074ba4
Kjiw0n <rwd0904@naver.com>
2024-01-05 14:17:40 +0900

    Feat: OpenAI API 연결

---
+++ src/main/java/com/kuit/chatdiary/controller/OpenAIController.java
@@ -0,0 +1,27 @@
+package com.kuit.chatdiary.controller;
+
+import com.kuit.chatdiary.service.OpenAIService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+
+@RestController
+@RequiredArgsConstructor
+public class OpenAIController {
+
+    @Autowired
+    private final OpenAIService openAIService;
+
+    @GetMapping("/gpt")
+    public ResponseEntity<?> openAI() {
+        try {
+            return ResponseEntity.ok().body(openAIService.getCompletion("안녕"));
+        } catch (HttpClientErrorException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+
+}
