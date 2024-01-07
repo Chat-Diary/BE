30cbbebfe88ca97f44a9e6249f97227b9e6248d8
Mouon <137624597+Mouon@users.noreply.github.com>
2024-01-07 18:05:59 +0900

    Feat: Chat-133-테스트 페이지 추가 (#10)
    * Feat: 테스트 페이지 추가
    
    chat.html추가
    UserController 리다이렉션 수정
    gradle파일에 의존성 추가
    
    * Chore : git ignore 재적용
    
    * Feat : endpoint url test로 수정 & Key 공개 실수 force push
    
    ---------
    
    Co-authored-by: nonaninona <starcraft0529@gmail.com>

--- src/main/java/com/kuit/chatdiary/controller/UserController.java
+++ src/main/java/com/kuit/chatdiary/controller/UserController.java
@@ -1,12 +1,17 @@
 package com.kuit.chatdiary.controller;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class UserController {
 
+    @GetMapping("/testChat")
+    public String chat() {
+        return "redirect:/testChat.html";
+    }
     @GetMapping("/user")
     public String user() {
-        return "user";
+        return "redirect:/user.html";
     }
 }
