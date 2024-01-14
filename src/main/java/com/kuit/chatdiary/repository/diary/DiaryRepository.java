93183ba6b26251cfc7315256d6942f7b663fa122
Mouon <ahemsapsldk@gmail.com>
2024-01-14 12:01:42 +0900

    Feat: 날짜를 기반으로한 일기라스트 조회
    DiaryInquiryDto 작성
    DiaryInquiryService 작성

---
+++ src/main/java/com/kuit/chatdiary/repository/diary/DiaryRepository.java
@@ -0,0 +1,10 @@
+package com.kuit.chatdiary.repository.diary;
+
+import com.kuit.chatdiary.domain.Diary;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DiaryRepository  {
+
+}
