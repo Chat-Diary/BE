36cc34f6559b2cc66f376ca36b6d1247f3d9ad5d
dainshon <shon1107@naver.com>
2024-01-14 17:59:15 +0900

    Fix: 컬럼명 바뀐거 수정, @MappedSuperclass 어노테이션 삭제

--- src/main/java/com/kuit/chatdiary/domain/Diary.java
+++ src/main/java/com/kuit/chatdiary/domain/Diary.java
@@ -19,6 +19,7 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
+
 public class Diary {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
