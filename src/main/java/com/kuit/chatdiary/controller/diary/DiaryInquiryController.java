7255394aa2173d118b72059bdc8a2e045c1ad345
Mouon <ahemsapsldk@gmail.com>
2024-01-14 12:01:42 +0900

    Feat: 날짜를 기반으로한 일기라스트 조회
    DiaryInquiryDto 작성
    DiaryInquiryService 작성

---
+++ src/main/java/com/kuit/chatdiary/controller/diary/DiaryInquiryController.java
@@ -0,0 +1,34 @@
+package com.kuit.chatdiary.controller.diary;
+
+import com.kuit.chatdiary.domain.Diary;
+import com.kuit.chatdiary.dto.diary.DiaryInquiryRequest;
+import com.kuit.chatdiary.repository.diary.DiaryInquiryRepository;
+import com.kuit.chatdiary.repository.diary.DiaryRepository;
+import com.kuit.chatdiary.service.diary.DiaryInquiryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+public class DiaryInquiryController {
+
+    private final DiaryInquiryService diaryInquiryService;
+
+    public DiaryInquiryController(DiaryInquiryService diaryInquiryService){
+        this.diaryInquiryService = diaryInquiryService;
+    }
+
+    @GetMapping("/getDailyEvents")
+    public ResponseEntity<List<Diary>> getDiaryList(@RequestParam("user_id") Long user_id, @RequestParam("day") String day) {
+            LocalDate selectedDate = LocalDate.parse(day);
+            List<Diary> events = diaryInquiryService.getDiaryWithPhotos(user_id, selectedDate);
+            return ResponseEntity.ok(events);
+
+    }
+
+}
