c0912f66355e7990ec1033f27a932fdc685cfc3d
Mouon <ahemsapsldk@gmail.com>
2024-01-16 21:48:10 +0900

    Feat: DiaryInquiry Monthiy 조회로 로직 수정

--- src/main/java/com/kuit/chatdiary/repository/diary/DiaryInquiryRepository.java
+++ src/main/java/com/kuit/chatdiary/repository/diary/DiaryInquiryRepository.java
@@ -31,4 +31,12 @@ public class DiaryInquiryRepository implements DiaryRepository {
                 .getResultList();
     }
 
+    public List<Diary> inquiryDiaryRange(Long userId, LocalDate startDate, LocalDate endDate) {
+        return em.createQuery("SELECT d FROM diary d LEFT JOIN FETCH d.diaryTagList WHERE d.member.userId = :userId AND d.day BETWEEN :startDate AND :endDate", Diary.class)
+                .setParameter("userId", userId)
+                .setParameter("startDate", startDate.toString())
+                .setParameter("endDate", endDate.toString())
+                .getResultList();
+    }
+
 }
