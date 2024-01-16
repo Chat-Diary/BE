aa9e9b7213563a35a0d200719a2f0dbd8ccdfa9a
Mouon <ahemsapsldk@gmail.com>
2024-01-16 21:34:57 +0900

    Feat: CalendarInquiryRepository 수정
    DTO 삭제

--- src/main/java/com/kuit/chatdiary/dto/CalendarInquiryRequest.java
+++ src/main/java/com/kuit/chatdiary/dto/CalendarInquiryRequest.java
@@ -1,4 +1,19 @@
 package com.kuit.chatdiary.dto;
 
+import com.kuit.chatdiary.domain.Member;
+import com.kuit.chatdiary.domain.Sender;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class CalendarInquiryRequest {
+
+    private Member member;
+    private LocalDateTime createAt;
+
 }
