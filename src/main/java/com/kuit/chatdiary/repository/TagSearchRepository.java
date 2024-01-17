1da7aebb305c54c3ad6924fa1487b25837389567
Mouon <ahemsapsldk@gmail.com>
2024-01-18 01:23:17 +0900

    Feat: 태그 검색 API 작성

---
+++ src/main/java/com/kuit/chatdiary/repository/TagSearchRepository.java
@@ -0,0 +1,43 @@
+package com.kuit.chatdiary.repository;
+
+import com.kuit.chatdiary.domain.Diary;
+import com.kuit.chatdiary.domain.DiaryTag;
+import com.kuit.chatdiary.dto.TagSearchResponse;
+import jakarta.persistence.EntityManager;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Repository
+public class TagSearchRepository {
+
+    private final EntityManager em;
+
+
+    public TagSearchRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    public List<TagSearchResponse> findByTag(String tagName){
+        List<Diary> diaries = em.createQuery("select d from diary d join d.diaryTagList dt where dt.tag.tagName = :tagName", Diary.class)
+                .setParameter("tagName",tagName)
+                .getResultList();
+
+        return diaries.stream().map(diary -> {
+            TagSearchResponse response = new TagSearchResponse();
+            response.setDiaryId(diary.getDiaryId());
+            response.setTitle(diary.getTitle());
+            response.setContent(diary.getContent());
+            response.setDiaryDate(diary.getDiaryDate());
+            response.setCreateAt(diary.getCreateAt());
+            response.setUpdateAt(diary.getUpdateAt());
+            response.setStatus(diary.getStatus());
+            response.setTagList(diary.getDiaryTagList().stream().map(diaryTag -> diaryTag.getTag().getTagName()).collect(Collectors.toList()));
+            response.setPhotoCount(diary.getPhotoList().size());
+            return response;
+        }).collect(Collectors.toList());
+    }
+
+}
