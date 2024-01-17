dd386817ba9164b61a1b3cfbd77001ac41729d99
Mouon <ahemsapsldk@gmail.com>
2024-01-18 01:37:01 +0900

    Fix: 레포지토리에서 엔티티가 쿼리문 반환타입이게 수정

--- src/main/java/com/kuit/chatdiary/dto/diary/DiaryInquiryResponse.java
+++ src/main/java/com/kuit/chatdiary/dto/diary/DiaryInquiryResponse.java
@@ -1,16 +1,23 @@
 package com.kuit.chatdiary.dto.diary;
 
+import com.kuit.chatdiary.domain.DiaryTag;
+import com.kuit.chatdiary.domain.Photo;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class DiaryInquiryResponse {
     private Long diary_id;
     private Long user_id;
-    private String day;
-    private List<String> tags;
-    private String representativePhotoUrl;
+    private String title;
+    private String diaryDate;
+    private List<DiaryTag> diaryTagList = new ArrayList<>();
+    private List<Photo> photoList = new ArrayList<>();
 }
--- src/main/java/com/kuit/chatdiary/repository/diary/DiaryInquiryRepository.java
+++ src/main/java/com/kuit/chatdiary/repository/diary/DiaryInquiryRepository.java
@@ -1,34 +1,48 @@
 package com.kuit.chatdiary.repository.diary;
 
 import com.kuit.chatdiary.domain.Diary;
-import com.kuit.chatdiary.domain.Photo;
+import com.kuit.chatdiary.dto.diary.DiaryInquiryResponse;
 import jakarta.persistence.EntityManager;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
-public class DiaryInquiryRepository implements DiaryRepository {
+public class DiaryInquiryRepository {
     private final EntityManager em;
     public  DiaryInquiryRepository(EntityManager em){
         this.em=em;
     }
 
 
-    public List<Diary> inquiryDiary(Long userId, LocalDate diaryDate) {
-        return em.createQuery("SELECT d FROM diary d LEFT JOIN FETCH d.diaryTagList WHERE d.member.userId = :userId AND d.diaryDate = :diaryDate", Diary.class)
+    public List<DiaryInquiryResponse> inquiryDiary(Long userId, LocalDate diaryDate) {
+        return em.createQuery("SELECT d FROM diary d LEFT JOIN FETCH d.diaryTagList WHERE d.member.userId = :userId AND d.diaryDate = :diaryDate", DiaryInquiryResponse.class)
                 .setParameter("userId", userId)
                 .setParameter("diaryDate", diaryDate.toString())
                 .getResultList();
     }
 
-    public List<Diary> inquiryDiaryRange(Long userId, LocalDate startDate, LocalDate endDate) {
-        return em.createQuery("SELECT d FROM diary d LEFT JOIN FETCH d.diaryTagList WHERE d.member.userId = :userId AND d.diaryDate BETWEEN :startDate AND :endDate", Diary.class)
+    public List<DiaryInquiryResponse> inquiryDiaryRange(Long userId, LocalDate startDate, LocalDate endDate) {
+        List<Diary> diaries=em.createQuery("SELECT d FROM diary d LEFT JOIN FETCH d.diaryTagList WHERE d.member.userId = :userId AND d.diaryDate BETWEEN :startDate AND :endDate", Diary.class)
                 .setParameter("userId", userId)
                 .setParameter("startDate", startDate.toString())
                 .setParameter("endDate", endDate.toString())
                 .getResultList();
+        return diaries.stream().map(diary -> {
+            DiaryInquiryResponse response = new DiaryInquiryResponse();
+            response.setDiary_id(diary.getMember().getUserId());
+            response.setDiary_id(diary.getDiaryId());
+            response.setTitle(diary.getTitle());
+            response.setDiaryDate(diary.getDiaryDate());
+            response.setDiaryTagList(diary.getDiaryTagList());
+            response.setPhotoList(diary.getPhotoList());
+
+            return response;
+        }).collect(Collectors.toList());
+
     }
+
 
 }
