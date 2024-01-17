7e99ef4430ac3b3cbb415e0568cb2158a0f87c5d
nonaninona <53222283+nonaninona@users.noreply.github.com>
2024-01-17 22:49:07 +0900

    Feat: Chat-161-be-erd-코드화(Squash and Merge 용) (#13)
    * test commit
    
    * erd 기반 domain 생성
    
    * FEAT: erd 기반 domain 패키지 생성
    
    * Feat: erd 기반 domain 패키지 생성
    
    * Refactor: 생성시간 type 수정(String->LocalDateTime)
    
    * Refactor: 생성시간 type 수정(String->LocalDateTime)
    
    * Fix: 컬럼명 바뀐거 수정, @MappedSuperclass 어노테이션 삭제
    
    * Fix: 컬럼명 카멜 표기법으로 수정
    
    * Fix: @ColumnDefault 삭제, Sender에 index로 찾는 함수 추가
    
    * Fix: local db로 수정
    
    * Fix: @ColumnDefault 내부 값 ''로 감싸기
    
    * Feat : application.yml의 active profile을 dev로 변경
    
    * Fix : Test build failed 해결
    
    Test 코드 주석처리
    
    * Fix: ChatType을 ENUM 으로 변경
    
    * Fix: chatType 변수 ChatType enum으로 변경
    
    * Feat : docker rm 추가 및 환경변수 설정, application 환경변수 설정
    
    docker container를 중지시키는 rm 코드 추가
    docker container 빌드 시 GitHub Secret을 이용해 환경변수 주입
    application active profile을 환경변수로 설정하여 local과 docker에서 다른 값을 가지도록 유도
    
    * Fix : docker 환경변수 추가 구문 수정
    
    \ 추가
    
    * Fix: 변수명 변경(day->diaryDate)
    
    ---------
    
    Co-authored-by: dainshon <shon1107@naver.com>
    Co-authored-by: Mouon <137624597+mouon@users.noreply.github.com>

---
+++ src/main/java/com/kuit/chatdiary/domain/Chat.java
@@ -0,0 +1,45 @@
+package com.kuit.chatdiary.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "chat")
+@Getter
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Chat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "chat_id")
+    private Long chatId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id")
+    private Member member;
+
+    @Enumerated(EnumType.STRING)
+    @ColumnDefault("'USER'")
+    private Sender sender;
+
+    private String content;
+
+    @CreatedDate
+    @Column(name = "create_at")
+    private LocalDateTime createAt;
+
+    @Enumerated(EnumType.STRING)
+    @ColumnDefault("'CHAT'")
+    @Column(name = "chat_type")
+    private ChatType chatType;
+
+
+
+
+}
