122808378d232f8c9ae0a3c40f7915d0bbf86eb8
Mouon <ahemsapsldk@gmail.com>
2024-01-17 22:45:53 +0900

    Feat: DiaryInquiryRepositoryInquiry 수정
    엔드포인트 수정, DTO를 통해 컨트롤러 계층에 Domain계층 노출 없앰

--- src/main/java/com/kuit/chatdiary/controller/diary/DiaryInquiryController.java
+++ src/main/java/com/kuit/chatdiary/controller/diary/DiaryInquiryController.java
@@ -1,11 +1,8 @@
 package com.kuit.chatdiary.controller.diary;
 
 import com.kuit.chatdiary.domain.Diary;
-import com.kuit.chatdiary.dto.diary.DiaryInquiryRequest;
-import com.kuit.chatdiary.repository.diary.DiaryInquiryRepository;
-import com.kuit.chatdiary.repository.diary.DiaryRepository;
+import com.kuit.chatdiary.dto.diary.DiaryInquiryResponse;
 import com.kuit.chatdiary.service.diary.DiaryInquiryService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,18 +22,18 @@ public class DiaryInquiryController {
 
 
     @GetMapping("/getDailyEvents")
-    public ResponseEntity<List<Diary>> getDiaryList(@RequestParam("user_id") Long user_id, @RequestParam("day") String day) {
+    public ResponseEntity<List<DiaryInquiryResponse>> getDiaryList(@RequestParam("user_id") Long user_id, @RequestParam("day") String day) {
             LocalDate selectedDate = LocalDate.parse(day);
-            List<Diary> events = diaryInquiryService.getDiaryWithPhotos(user_id, selectedDate);
+            List<DiaryInquiryResponse> events = diaryInquiryService.getDiaryWithPhotos(user_id, selectedDate);
             return ResponseEntity.ok(events);
 
     }
 
     @GetMapping("/get-monthly-diary")
-    public ResponseEntity<List<Diary>> getMonthlyDiaryList(@RequestParam("user_id") Long user_id,
+    public ResponseEntity<List<DiaryInquiryResponse>> getMonthlyDiaryList(@RequestParam("user_id") Long user_id,
                                                            @RequestParam("year") int year,
                                                            @RequestParam("month") int month){
-        List<Diary> monthlyEvents = diaryInquiryService.getMonthlyDiaryPhotos(user_id,year,month);
+        List<DiaryInquiryResponse> monthlyEvents = diaryInquiryService.getMonthlyDiaryPhotos(user_id,year,month);
         return ResponseEntity.ok(monthlyEvents);
     }
 
--- src/main/java/com/kuit/chatdiary/dto/diary/DiaryInquiryRequest.java
+++ src/main/java/com/kuit/chatdiary/dto/diary/DiaryInquiryRequest.java
@@ -1,7 +1,11 @@
 package com.kuit.chatdiary.dto.diary;
 
+import com.kuit.chatdiary.domain.DiaryTag;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -9,7 +13,8 @@ public class DiaryInquiryRequest {
 
     private Long diary_id;
     private Long user_id;
-    private String day;
+    private String diaryDate;
     private String title;
     private String content;
+
 }
