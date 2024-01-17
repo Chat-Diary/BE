0cff36985fa818f202ecbe24c81f386667aca26e
Mouon <ahemsapsldk@gmail.com>
2024-01-17 11:47:32 +0900

    Feat: DiaryInquiryRepositoryInquiry 수정
    바뀐 도메인에 맞게 수정

--- src/main/java/com/kuit/chatdiary/repository/diary/DiaryInquiryRepository.java
+++ src/main/java/com/kuit/chatdiary/repository/diary/DiaryInquiryRepository.java
@@ -16,15 +16,15 @@ public class DiaryInquiryRepository implements DiaryRepository {
     }
 
 
-    public List<Diary> inquiryDiary(Long userId, LocalDate day) {
-        return em.createQuery("SELECT d FROM diary d LEFT JOIN FETCH d.diaryTagList WHERE d.member.userId = :userId AND d.day = :day", Diary.class)
+    public List<Diary> inquiryDiary(Long userId, LocalDate diaryDate) {
+        return em.createQuery("SELECT d FROM diary d LEFT JOIN FETCH d.diaryTagList WHERE d.member.userId = :userId AND d.diaryDate = :diaryDate", Diary.class)
                 .setParameter("userId", userId)
-                .setParameter("day", day.toString())
+                .setParameter("diaryDate", diaryDate.toString())
                 .getResultList();
     }
 
     public List<Diary> inquiryDiaryRange(Long userId, LocalDate startDate, LocalDate endDate) {
-        return em.createQuery("SELECT d FROM diary d LEFT JOIN FETCH d.diaryTagList WHERE d.member.userId = :userId AND d.day BETWEEN :startDate AND :endDate", Diary.class)
+        return em.createQuery("SELECT d FROM diary d LEFT JOIN FETCH d.diaryTagList WHERE d.member.userId = :userId AND d.diaryDate BETWEEN :startDate AND :endDate", Diary.class)
                 .setParameter("userId", userId)
                 .setParameter("startDate", startDate.toString())
                 .setParameter("endDate", endDate.toString())
