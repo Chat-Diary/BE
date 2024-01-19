eccfe038a386fe50c2aef2a6ca3cb58258da3a0f
Mouon <ahemsapsldk@gmail.com>
2024-01-19 21:17:33 +0900

    Fix: Develop 변경 사항에 맞게 수정해서 브렌치 생성

---
+++ src/main/java/com/kuit/chatdiary/controller/diary/DiaryInquiryController.java
@@ -0,0 +1,41 @@
+package com.kuit.chatdiary.controller.diary;
+
+
+import com.kuit.chatdiary.dto.diary.DiaryInquiryResponse;
+import com.kuit.chatdiary.service.diary.DiaryInquiryService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+public class DiaryInquiryController {
+
+    private final DiaryInquiryService diaryInquiryService;
+
+    public DiaryInquiryController(DiaryInquiryService diaryInquiryService){
+        this.diaryInquiryService = diaryInquiryService;
+    }
+
+
+    @GetMapping("/getDailyEvents")
+    public ResponseEntity<List<DiaryInquiryResponse>> getDiaryList(@RequestParam("user_id") Long user_id, @RequestParam("day") String day) {
+        LocalDate selectedDate = LocalDate.parse(day);
+        List<DiaryInquiryResponse> events = diaryInquiryService.getDiaryWithPhotos(user_id, selectedDate);
+        return ResponseEntity.ok(events);
+
+    }
+
+    @GetMapping("/get-monthly-diary")
+    public ResponseEntity<List<DiaryInquiryResponse>> getMonthlyDiaryList(@RequestParam("user_id") Long user_id,
+                                                                          @RequestParam("year") int year,
+                                                                          @RequestParam("month") int month){
+        List<DiaryInquiryResponse> monthlyEvents = diaryInquiryService.getMonthlyDiaryPhotos(user_id,year,month);
+        return ResponseEntity.ok(monthlyEvents);
+    }
+
+
+}
---
+++ src/main/java/com/kuit/chatdiary/dto/diary/DiaryInquiryResponse.java
@@ -0,0 +1,24 @@
+package com.kuit.chatdiary.dto.diary;
+
+import com.kuit.chatdiary.domain.DiaryTag;
+import com.kuit.chatdiary.domain.Photo;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class DiaryInquiryResponse {
+    private Long diary_id;
+    private Long user_id;
+    private String title;
+    private Date diaryDate;
+    private List<DiaryTag> diaryTagList = new ArrayList<>();
+    private List<Photo> photoList = new ArrayList<>();
+}
