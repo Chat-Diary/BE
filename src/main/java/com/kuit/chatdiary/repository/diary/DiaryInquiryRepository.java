f88d9a9c5477be688f7eb74fd3cd0bc3890c86f8
Mouon <ahemsapsldk@gmail.com>
2024-01-19 21:17:33 +0900

    Fix: Develop 변경 사항에 맞게 수정해서 브렌치 생성

---
+++ src/main/java/com/kuit/chatdiary/repository/diary/DiaryInquiryRepository.java
@@ -0,0 +1,49 @@
+package com.kuit.chatdiary.repository.diary;
+
+import com.kuit.chatdiary.domain.Diary;
+import com.kuit.chatdiary.dto.diary.DiaryInquiryResponse;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TemporalType;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Repository
+public class DiaryInquiryRepository {
+    private final EntityManager em;
+    public  DiaryInquiryRepository(EntityManager em){
+        this.em=em;
+    }
+
+
+    public List<DiaryInquiryResponse> inquiryDiary(Long userId, LocalDate diaryDate) {
+        return em.createQuery("SELECT d FROM diary d LEFT JOIN FETCH d.diaryTagList WHERE d.member.userId = :userId AND d.diaryDate = :diaryDate", DiaryInquiryResponse.class)
+                .setParameter("userId", userId)
+                .setParameter("diaryDate", diaryDate.toString())
+                .getResultList();
+    }
+
+    public List<DiaryInquiryResponse> inquiryDiaryRange(Long userId, Date startDate, Date endDate) {
+        List<Diary> diaries=em.createQuery("SELECT d FROM diary d LEFT JOIN FETCH d.diaryTagList WHERE d.member.userId = :userId AND d.diaryDate BETWEEN :startDate AND :endDate", Diary.class)
+                .setParameter("userId", userId)
+                .setParameter("startDate", startDate, TemporalType.DATE)
+                .setParameter("endDate", endDate,TemporalType.DATE)
+                .getResultList();
+        return diaries.stream().map(diary -> {
+            DiaryInquiryResponse response = new DiaryInquiryResponse();
+            response.setDiary_id(diary.getMember().getUserId());
+            response.setDiary_id(diary.getDiaryId());
+            response.setTitle(diary.getTitle());
+            response.setDiaryDate(diary.getDiaryDate());
+            response.setDiaryTagList(diary.getDiaryTagList());
+            response.setPhotoList(diary.getPhotoList());
+            return response;
+        }).collect(Collectors.toList());
+
+    }
+
+
+}
---
+++ src/main/java/com/kuit/chatdiary/service/diary/DiaryInquiryService.java
@@ -0,0 +1,43 @@
+package com.kuit.chatdiary.service.diary;
+
+import com.kuit.chatdiary.dto.diary.DiaryInquiryResponse;
+import com.kuit.chatdiary.repository.diary.DiaryInquiryRepository;
+import org.springframework.stereotype.Service;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class DiaryInquiryService {
+
+    private final DiaryInquiryRepository diaryInquiryRepository;
+
+    public DiaryInquiryService(DiaryInquiryRepository diaryInquiryRepository){
+        this.diaryInquiryRepository=diaryInquiryRepository;
+    }
+
+    /**
+     * diary.getPhotoList().size(); 를 통해 jpa가 각 객체의 사진 목록 로드
+     * */
+    public List<DiaryInquiryResponse> getDiaryWithPhotos(Long userId, LocalDate date){
+        List<DiaryInquiryResponse> diaries = diaryInquiryRepository.inquiryDiary(userId, date);
+        diaries.forEach(diary -> {
+            diary.getPhotoList().size();
+        });
+        return diaries;
+    }
+
+    public List<DiaryInquiryResponse> getMonthlyDiaryPhotos(Long userId, int year,int month){
+        LocalDate firstDayOfMonthLocal =LocalDate.of(year,month,1);
+        LocalDate lastDayOfMonthLocal =firstDayOfMonthLocal.plusMonths(1).minusDays(1);
+
+        Date firstDayOfMonth = Date.valueOf(firstDayOfMonthLocal);
+        Date lastDayOfMonth = Date.valueOf(lastDayOfMonthLocal);
+
+        List<DiaryInquiryResponse> diaries = diaryInquiryRepository.inquiryDiaryRange(userId, firstDayOfMonth, lastDayOfMonth);
+        diaries.forEach(diary -> diary.getPhotoList().size());
+        return diaries;
+    }
+
+}
