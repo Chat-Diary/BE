38a523dcf0e860fd865abbf335d4e5cf86337aca
Mouon <ahemsapsldk@gmail.com>
2024-01-20 12:52:27 +0900

    Feat: 갤린더 조회 API 작성
    깃 꼬임으로 인해 새로운 브렌치

---
+++ src/main/java/com/kuit/chatdiary/dto/CalendarInquiryResponse.java
@@ -0,0 +1,15 @@
+package com.kuit.chatdiary.dto;
+
+import com.kuit.chatdiary.domain.Sender;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CalendarInquiryResponse {
+
+    private Sender sender;
+    private boolean exists;
+}
