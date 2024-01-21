bcdfdc53ed9a22a900c0ae9cb5a09ec534749811
Mouon <ahemsapsldk@gmail.com>
2024-01-21 23:11:32 +0900

    Fix: 도메인 형식 맞춰서 엔드 포인트 수정

--- src/main/java/com/kuit/chatdiary/controller/diary/DiaryInquiryController.java
+++ src/main/java/com/kuit/chatdiary/controller/diary/DiaryInquiryController.java
@@ -5,6 +5,7 @@ import com.kuit.chatdiary.dto.diary.DiaryInquiryResponse;
 import com.kuit.chatdiary.service.diary.DiaryInquiryService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @RestController
+@RequestMapping("diary")
 public class DiaryInquiryController {
 
     private final DiaryInquiryService diaryInquiryService;
@@ -20,7 +22,7 @@ public class DiaryInquiryController {
         this.diaryInquiryService = diaryInquiryService;
     }
 
-    @GetMapping("/get-monthly-diary")
+    @GetMapping("/get_monthly_diary")
     public ResponseEntity<List<DiaryInquiryResponse>> getMonthlyDiaryList(@RequestParam("user_id") Long user_id,
                                                                           @RequestParam("year") int year, @RequestParam("month") int month){
         List<DiaryInquiryResponse> monthlyEvents = diaryInquiryService.getMonthlyDiaryPhotos(user_id,year,month);
