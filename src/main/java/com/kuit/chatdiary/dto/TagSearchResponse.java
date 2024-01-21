24b524ef44f4fa74005d1f5348f535d16df74fd0
Mouon <ahemsapsldk@gmail.com>
2024-01-21 12:53:40 +0900

    Feat: TagSearchResponse 작성
    태그 검색을 위한 DTO 작성

---
+++ src/main/java/com/kuit/chatdiary/dto/TagSearchResponse.java
@@ -0,0 +1,25 @@
+package com.kuit.chatdiary.dto;
+
+import com.kuit.chatdiary.domain.Photo;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class TagSearchResponse {
+    private Long diaryId;
+    private String title;
+    private String content;
+    private Date diaryDate;
+    private LocalDateTime createAt;
+    private LocalDateTime updateAt;
+    private String status;
+    private List<String> tagList;
+    private List<Photo> photoList;
+}
