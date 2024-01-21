db239cf0bdf02d3f64520a3e8fbc15cf7af5dbf0
Mouon <ahemsapsldk@gmail.com>
2024-01-21 16:59:30 +0900

    Fix: CalendarInquiryController 수정
    파라메터의 날짜 형식을 지정하고, 날짜별로 sender와 그에 대응하는 boolean을 매핑하였습니다.
    
    날짜별 sender의 존재를 확인할 수 있습니다. 맵으로
    json보낼때 map이 뭔가 좋아보여 map위주로 구성했습니다.

--- src/main/java/com/kuit/chatdiary/controller/calendar/CalendarInquiryController.java
+++ src/main/java/com/kuit/chatdiary/controller/calendar/CalendarInquiryController.java
@@ -6,29 +6,29 @@ import com.kuit.chatdiary.service.calendar.CalendarInquiryService;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.YearMonth;
 import java.util.List;
 import java.util.Map;
 
 @RestController
+@RequestMapping("chat")
 public class CalendarInquiryController {
     private final CalendarInquiryService calendarInquiryService;
     public CalendarInquiryController(CalendarInquiryService calendarInquiryService) {
         this.calendarInquiryService = calendarInquiryService;
     }
-    @GetMapping("/chat-exists")
-    public ResponseEntity<List<CalendarInquiryResponse>> getChatExistsByDate(
+    @GetMapping("/chat-exists-month")
+    public ResponseEntity<Map<LocalDate, Map<Sender, Boolean>>> getChatExistsByMonth(
             @RequestParam("memberId") long memberId,
-            @RequestParam("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate day) {
-        List<CalendarInquiryResponse> chatExists = calendarInquiryService.existsChatByDate(memberId, day);
+            @RequestParam("month") @DateTimeFormat(pattern = "yyyy-MM") YearMonth month) {
+        Map<LocalDate, Map<Sender, Boolean>> chatExistsByMonth = calendarInquiryService.existsChatByMonth(memberId, month);
 
-        return ResponseEntity.ok(chatExists);
+        return ResponseEntity.ok(chatExistsByMonth);
     }
-
-
-
 }
