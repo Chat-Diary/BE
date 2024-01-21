6866ef971516a49213f78c700d65762c02aaf205
Mouon <ahemsapsldk@gmail.com>
2024-01-21 12:44:46 +0900

    Feat: Calendar Controller 작성
    요청시 날짜와 유저 아이디를 기준으로 선택된 날짜의 채팅 내역을 알 수 있게 작성

---
+++ src/main/java/com/kuit/chatdiary/controller/calendar/CalendarInquiryController.java
@@ -0,0 +1,34 @@
+package com.kuit.chatdiary.controller.calendar;
+
+import com.kuit.chatdiary.domain.Sender;
+import com.kuit.chatdiary.dto.CalendarInquiryResponse;
+import com.kuit.chatdiary.service.calendar.CalendarInquiryService;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class CalendarInquiryController {
+    private final CalendarInquiryService calendarInquiryService;
+    public CalendarInquiryController(CalendarInquiryService calendarInquiryService) {
+        this.calendarInquiryService = calendarInquiryService;
+    }
+    @GetMapping("/chat-exists")
+    public ResponseEntity<List<CalendarInquiryResponse>> getChatExistsByDate(
+            @RequestParam("memberId") long memberId,
+            @RequestParam("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate day) {
+        List<CalendarInquiryResponse> chatExists = calendarInquiryService.existsChatByDate(memberId, day);
+
+        return ResponseEntity.ok(chatExists);
+    }
+
+
+
+}
