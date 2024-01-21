85f5e3e0768aaf03f3c6643e9ea31e90b7a9c9f1
Mouon <ahemsapsldk@gmail.com>
2024-01-21 16:56:50 +0900

    Fix: CalendarInquiryRepository 수정
    기존에 일별로 받아서 프론트 입장을 고려를 못했는데, 월별로 받아서 해결했습니다.
    
    endOfMonth.atTime(LocalTime.MAX) 이거
    artOfMonth.atStartOfDay() 이런 자바에서 제공하는 기능을 활용했습니다.
    atTime(LocalTime.MAX)은 마지막날의 자정까지 포함 하도록 구현한것입니다. (치다가 자정으로 넘어가버리는 채팅 특성을 고려)

--- src/main/java/com/kuit/chatdiary/repository/CalendarInquiryRepository.java
+++ src/main/java/com/kuit/chatdiary/repository/CalendarInquiryRepository.java
@@ -6,6 +6,9 @@ import jakarta.persistence.EntityManager;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.YearMonth;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,23 +19,43 @@ public class CalendarInquiryRepository {
     public CalendarInquiryRepository(EntityManager em) {
         this.em = em;
     }
-    public Map<Sender, Boolean> existsChatByDate(long memberId, LocalDate day){
-        List<Object[]> results = em.createQuery("select c.sender, count(c) from chat c where c.member.userId = :userId and c.createAt between :startOfDay and :endOfDay group by c.sender", Object[].class)
+    public Map<LocalDate, Map<Sender, Boolean>> existsChatByMonth(long memberId, YearMonth month) {
+        LocalDate startOfMonth = month.atDay(1);
+        LocalDate endOfMonth = month.atEndOfMonth();
+
+        /**
+         * 쿼리 넘 길어져서 + 로 붙임요
+         * */
+        List<Object[]> results = em.createQuery(
+                        "SELECT CAST(c.createAt AS date), c.sender " +
+                                "FROM chat c WHERE c.member.userId = :userId " +
+                                "AND c.createAt BETWEEN :startOfMonth AND :endOfMonth " +
+                                "GROUP BY CAST(c.createAt AS date), c.sender", Object[].class)
                 .setParameter("userId", memberId)
-                .setParameter("startOfDay", day.atStartOfDay())
-                .setParameter("endOfDay", day.plusDays(1).atStartOfDay())
+                .setParameter("startOfMonth", startOfMonth.atStartOfDay())
+                .setParameter("endOfMonth", endOfMonth.atTime(LocalTime.MAX))// 하루 끝 포함 자바 time 라이브러리 굿 ㅋㅋ
                 .getResultList();
 
-        Map<Sender, Boolean> senderExistsMap = new HashMap<>();
-        for (Sender sender : Sender.values()) {
-            senderExistsMap.put(sender, false);
+        Map<LocalDate, Map<Sender, Boolean>> chatExistsByMonth = new HashMap<>();
+
+        for (LocalDate date = startOfMonth; !date.isAfter(endOfMonth); date = date.plusDays(1)) {
+            Map<Sender, Boolean> dailySenderExists = new HashMap<>();
+            for (Sender sender : Sender.values()) {
+                dailySenderExists.put(sender, false);
+            }
+            chatExistsByMonth.put(date, dailySenderExists);
         }
 
         for (Object[] result : results) {
-            senderExistsMap.put((Sender) result[0], (Long) result[1] > 0);
+            LocalDate date = ((LocalDateTime) result[0]).toLocalDate();
+            Sender sender = (Sender) result[1];
+            Long count = (Long) result[2];
+            chatExistsByMonth.get(date).put(sender, count > 0);
         }
 
-        return senderExistsMap;
+        return chatExistsByMonth;
     }
+
+
 }
 
