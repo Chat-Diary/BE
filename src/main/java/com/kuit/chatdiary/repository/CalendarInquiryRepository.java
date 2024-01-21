0ac87226bb679b27b86e2f897adaf5bac60a9c3c
Mouon <ahemsapsldk@gmail.com>
2024-01-21 21:54:50 +0900

    Fix: 오류수정
    sql문에 오류가 발견되어 수정
    startOfMonth 대신
    startOfMonth.atStartOfDay()사용

--- src/main/java/com/kuit/chatdiary/repository/CalendarInquiryRepository.java
+++ src/main/java/com/kuit/chatdiary/repository/CalendarInquiryRepository.java
@@ -35,10 +35,6 @@ public class CalendarInquiryRepository {
             chatExistsByMonth.put(date, dailyResponses);
         }
 
-        /**
-         * 쿼리 문이 너무 길어져서 + 연산자 사용했습니다.
-         * CAST 쓴이유는 일부만 사용하려고..날짜 중에
-         * */
         List<Object[]> results = em.createQuery(
                         "SELECT CAST(c.createAt AS date), c.sender, COUNT(*) " +
                                 "FROM chat c WHERE c.member.userId = :userId " +
@@ -46,28 +42,26 @@ public class CalendarInquiryRepository {
                                 "GROUP BY CAST(c.createAt AS date), c.sender", Object[].class)
                 .setParameter("userId", memberId)
                 .setParameter("startOfMonth", startOfMonth.atStartOfDay())
-                .setParameter("endOfMonth", endOfMonth.atTime(LocalTime.MAX))//시간 자정을 기준으로 하기위해 .. 채팅 특성 반영
+                .setParameter("endOfMonth", endOfMonth.atTime(LocalTime.MAX))
                 .getResultList();
 
         log.info("Query results: {}", results);
         for (Object[] result : results) {
-            LocalDate date = (LocalDate) result[0]; //CAST(c.createAt AS date) 결과
-            Sender sender = (Sender) result[1]; //c.sender 결과
-            Long count = (Long) result[2]; //COUNT(*) 결과..<-요게 핵심
+            LocalDate date = ((java.sql.Date) result[0]).toLocalDate();
+            Sender sender = (Sender) result[1];
+            Long count = (Long) result[2];
 
-            List<CalendarInquiryResponse> dailyResponses = chatExistsByMonth.get(date.toString());
+            List<CalendarInquiryResponse> dailyResponses = chatExistsByMonth.get(date);
 
             if (dailyResponses != null) {
                 for (CalendarInquiryResponse response : dailyResponses) {
                     if (response.getSender() == sender) {
-                        response.setExists(count > 0);//카운트 해서 0보다 크면 true요 아니면 false요
+                        response.setExists(count > 0);
                         break;
                     }
                 }
             }
-
         }
         return chatExistsByMonth;
     }
 }
-
