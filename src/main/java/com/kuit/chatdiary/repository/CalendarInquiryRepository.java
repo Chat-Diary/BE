477d765f1ffdd196bf9d3d8c36fdaa90b49a5028
Mouon <ahemsapsldk@gmail.com>
2024-01-21 12:40:56 +0900

    Feat: Calendar repository 작성
    Map을 이용해서 각 sender와 존재유무를 매핑, boolean을 이용

---
+++ src/main/java/com/kuit/chatdiary/repository/CalendarInquiryRepository.java
@@ -0,0 +1,38 @@
+package com.kuit.chatdiary.repository;
+
+import com.kuit.chatdiary.domain.Chat;
+import com.kuit.chatdiary.domain.Sender;
+import jakarta.persistence.EntityManager;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class CalendarInquiryRepository {
+    private final EntityManager em;
+    public CalendarInquiryRepository(EntityManager em) {
+        this.em = em;
+    }
+    public Map<Sender, Boolean> existsChatByDate(long memberId, LocalDate day){
+        List<Object[]> results = em.createQuery("select c.sender, count(c) from chat c where c.member.userId = :userId and c.createAt between :startOfDay and :endOfDay group by c.sender", Object[].class)
+                .setParameter("userId", memberId)
+                .setParameter("startOfDay", day.atStartOfDay())
+                .setParameter("endOfDay", day.plusDays(1).atStartOfDay())
+                .getResultList();
+
+        Map<Sender, Boolean> senderExistsMap = new HashMap<>();
+        for (Sender sender : Sender.values()) {
+            senderExistsMap.put(sender, false);
+        }
+
+        for (Object[] result : results) {
+            senderExistsMap.put((Sender) result[0], (Long) result[1] > 0);
+        }
+
+        return senderExistsMap;
+    }
+}
+
