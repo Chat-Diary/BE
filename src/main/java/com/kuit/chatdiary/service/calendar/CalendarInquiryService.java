650b96be20cab6e053b06adcdb5710c8bb1c8dfa
Mouon <ahemsapsldk@gmail.com>
2024-01-21 12:43:18 +0900

    Feat: Calendar Service 작성
    Dto 이용해서  Calendar Service작성

---
+++ src/main/java/com/kuit/chatdiary/service/calendar/CalendarInquiryService.java
@@ -0,0 +1,33 @@
+package com.kuit.chatdiary.service.calendar;
+
+import com.kuit.chatdiary.domain.Sender;
+import com.kuit.chatdiary.dto.CalendarInquiryResponse;
+import org.springframework.stereotype.Service;
+import com.kuit.chatdiary.repository.CalendarInquiryRepository;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class CalendarInquiryService {
+
+    private final CalendarInquiryRepository calendarInquiryRepository;
+
+    public CalendarInquiryService(CalendarInquiryRepository calendarInquiryRepository) {
+        this.calendarInquiryRepository = calendarInquiryRepository;
+    }
+
+    public List<CalendarInquiryResponse> existsChatByDate(long memberId, LocalDate day){
+        Map<Sender, Boolean> senderExistsMap = calendarInquiryRepository.existsChatByDate(memberId, day);
+        List<CalendarInquiryResponse> senderChatExistsList = new ArrayList<>();
+
+        for (Map.Entry<Sender, Boolean> entry : senderExistsMap.entrySet()) {
+            senderChatExistsList.add(new CalendarInquiryResponse(entry.getKey(), entry.getValue()));
+        }
+
+        return senderChatExistsList;
+    }
+
+}
