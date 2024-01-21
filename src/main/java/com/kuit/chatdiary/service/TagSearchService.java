2b67d326e2165ea73a54094cf0cd4861c8f4b254
Mouon <ahemsapsldk@gmail.com>
2024-01-21 12:56:49 +0900

    Feat: TagSearchService 작성
    태그 이름을 통해 레포지토리를 호출하도록 설계

---
+++ src/main/java/com/kuit/chatdiary/service/TagSearchService.java
@@ -0,0 +1,20 @@
+package com.kuit.chatdiary.service;
+
+import com.kuit.chatdiary.dto.TagSearchResponse;
+import com.kuit.chatdiary.repository.TagSearchRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TagSearchService {
+    
+    private final TagSearchRepository tagSearchRepository;
+    public TagSearchService(TagSearchRepository tagSearchRepository) {
+        this.tagSearchRepository = tagSearchRepository;
+    }
+    public List<TagSearchResponse> findByTag(String tagName){
+        return tagSearchRepository.findByTag(tagName);
+    }
+
+}
