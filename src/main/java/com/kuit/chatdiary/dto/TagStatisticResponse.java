f7c859004b6cdae6a4b6f35658e04b2aa81518af
Mouon <ahemsapsldk@gmail.com>
2024-01-25 01:37:16 +0900

    Feat: TagStatisticResponse 추가
    레포지 작성을 위해 DTO 계층 추가, 태그이름 횟수 퍼센트 반영
    정렬은 프론트에서 처리하는 것이 맞다고 판단

---
+++ src/main/java/com/kuit/chatdiary/dto/TagStatisticResponse.java
@@ -0,0 +1,17 @@
+package com.kuit.chatdiary.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TagStatisticResponse {
+    private String tagName;
+    private Long count;
+    private double percentage;
+
+}
