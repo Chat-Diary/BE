1ce6f34096fb6e7812e3b534f9ed07f02987b5cc
Mouon <ahemsapsldk@gmail.com>
2024-01-27 21:08:19 +0900

    Feat: SenderController 작성
    컨트롤러 작성을 통해 엔드포인트 구현

--- src/main/java/com/kuit/chatdiary/controller/SenderController.java
+++ src/main/java/com/kuit/chatdiary/controller/SenderController.java
@@ -22,7 +22,6 @@ public class SenderController {
     public SenderController(SenderService senderService) {
         this.senderService = senderService;
     }
-
     @GetMapping("/sender")
     public ResponseEntity<List<ChatSenderStaticResponseDTO>> getTagStatistics(
             @RequestParam("memberId") Long memberId,
--- src/main/java/com/kuit/chatdiary/repository/ChatRepository.java
+++ src/main/java/com/kuit/chatdiary/repository/ChatRepository.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @Repository
 public class ChatRepository {
+
     private final EntityManager em;
 
     public ChatRepository(EntityManager em) {
