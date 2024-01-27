1c7f638f5ef922cd96de4f111bf9a08d1ba4aebb
Mouon <ahemsapsldk@gmail.com>
2024-01-27 20:57:14 +0900

    Feat: ChatSenderStaticResponseDTO등 작성
    레포지부터 DTO 작성

--- src/main/java/com/kuit/chatdiary/dto/chat/ChatSenderStaticResponseDTO.java
+++ src/main/java/com/kuit/chatdiary/dto/chat/ChatSenderStaticResponseDTO.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.sql.Date;
+
 @Getter
 @Setter
 @NoArgsConstructor
--- src/main/java/com/kuit/chatdiary/dto/diary/DateRangeDTO.java
+++ src/main/java/com/kuit/chatdiary/dto/diary/DateRangeDTO.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 
 import java.sql.Date;
 
+
 @AllArgsConstructor
 @Getter
 @Setter
