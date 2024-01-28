16c445da7316ff8f5fad1cda74e15e35f3282619
Mouon <ahemsapsldk@gmail.com>
2024-01-29 01:43:14 +0900

    Feat: 로직 수정
    diaryphoto가 추가됨에 따라 쿼리 로직 수정
    레포지 쪽 대폭 수정
    엔티티 나중에 머지 필요

--- src/main/java/com/kuit/chatdiary/dto/diary/TagSearchResponseDTO.java
+++ src/main/java/com/kuit/chatdiary/dto/diary/TagSearchResponseDTO.java
@@ -1,37 +1,25 @@
 package com.kuit.chatdiary.dto.diary;
 
-import com.kuit.chatdiary.domain.*;
-import lombok.*;
-
+import com.kuit.chatdiary.domain.Photo;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class TagSearchResponseDTO {
+
     private Long diaryId;
     private String title;
+    private String content;
     private Date diaryDate;
-    private List<TagInfoDTO> tagList;
-    private List<String> photoList;
-
-    @Builder
-    public TagSearchResponseDTO(Diary diary){
-        this.diaryId = diary.getDiaryId();
-        this.title = diary.getTitle();
-        this.diaryDate = diary.getDiaryDate();
-        this.tagList = diary.getDiaryTagList().stream()
-                .map(diaryTag -> {
-                    TagInfoDTO TagInfoDTO = new TagInfoDTO();
-                    TagInfoDTO.setTagId(diaryTag.getTag().getTagId());
-                    TagInfoDTO.setTagName(diaryTag.getTag().getTagName());
-                    return TagInfoDTO;
-                })
-                .collect(Collectors.toList());
-        this.photoList = diary.getPhotoList().stream()
-                .map(Photo::getImageUrl)
-                .collect(Collectors.toList());
-    }
+    private LocalDateTime createAt;
+    private LocalDateTime updateAt;
+    private String status;
+    private List<String> tagList;
+    private List<String> photoUrls;
 }
--- src/main/java/com/kuit/chatdiary/service/diary/TagSearchService.java
+++ src/main/java/com/kuit/chatdiary/service/diary/TagSearchService.java
@@ -1,14 +1,13 @@
 package com.kuit.chatdiary.service.diary;
 
 import com.kuit.chatdiary.dto.diary.TagSearchResponseDTO;
-import com.kuit.chatdiary.repository.TagSearchRepository;
+import com.kuit.chatdiary.repository.diary.TagSearchRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class TagSearchService {
-    
     private final TagSearchRepository tagSearchRepository;
     public TagSearchService(TagSearchRepository tagSearchRepository) {
         this.tagSearchRepository = tagSearchRepository;
