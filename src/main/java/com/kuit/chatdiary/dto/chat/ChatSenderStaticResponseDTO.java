0260b212a4a8c76e4ffbcf9b57065ecd9c85da19
Mouon <ahemsapsldk@gmail.com>
2024-01-30 13:29:30 +0900

    Fix : 충돌 해결
    충돌해결했습니다.

--- src/main/java/com/kuit/chatdiary/dto/chat/ChatSenderStaticResponseDTO.java
+++ src/main/java/com/kuit/chatdiary/dto/chat/ChatSenderStaticResponseDTO.java
@@ -12,6 +12,7 @@ import java.sql.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ChatSenderStaticResponseDTO {
+
     private Sender sender;
     private Long chatCount;
     private double percentage;
