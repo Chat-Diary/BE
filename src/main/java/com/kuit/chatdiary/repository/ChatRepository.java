01c6f1e05d175e87b64cbf3f38befd8be596f876
Mouon <ahemsapsldk@gmail.com>
2024-01-30 13:08:53 +0900

    Fix : 충돌 해결
    충돌해결했습니다.

--- src/main/java/com/kuit/chatdiary/repository/ChatRepository.java
+++ src/main/java/com/kuit/chatdiary/repository/ChatRepository.java
@@ -1,31 +1,11 @@
 package com.kuit.chatdiary.repository;
 
-import jakarta.persistence.EntityManager;
-import org.springframework.stereotype.Repository;
+import com.kuit.chatdiary.domain.Chat;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.sql.Date;
-import java.time.LocalDateTime;
 import java.util.List;
 
-@Repository
-public class ChatRepository {
-    private final EntityManager em;
+public interface ChatRepository extends JpaRepository<Chat, Long> {
 
-    public ChatRepository(EntityManager em) {
-        this.em = em;
-    }
-
-    public List<Object[]> countChatsBySenderAndDate(Long userId, Date startDate, Date endDate) {
-        String sql = "SELECT c.sender, COUNT(*) " +
-                "FROM chat c " +
-                "WHERE c.user_id = :userId " +
-                "AND c.create_at BETWEEN :startDate AND :endDate " +
-                "GROUP BY c.sender";
-
-        return em.createNativeQuery(sql)
-                .setParameter("userId", userId)
-                .setParameter("startDate", startDate)
-                .setParameter("endDate", endDate)
-                .getResultList();
-    }
+    List<Chat> findTop10ByMember_UserIdOrderByChatIdDesc(Long userId);
 }
