2ba38ee2109503e817b54d64380ce1746766fda0
Mouon <ahemsapsldk@gmail.com>
2024-01-30 13:22:35 +0900

    Fix : 충돌 해결
    충돌해결했습니다.

--- src/main/java/com/kuit/chatdiary/repository/ChatRepository.java
+++ src/main/java/com/kuit/chatdiary/repository/ChatRepository.java
@@ -4,7 +4,6 @@ import com.kuit.chatdiary.domain.Chat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-
 public interface ChatRepository extends JpaRepository<Chat, Long> {
 
     List<Chat> findTop10ByMember_UserIdOrderByChatIdDesc(Long userId);
