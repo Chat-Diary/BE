81a0629d373102298f7e5d26adb7e9b8a1cdac9d
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-01-31 21:40:35 +0900

    Feat : 서비스 계층및 컨트롤러 계층 작성
    레포지토리 사용

--- src/main/java/com/kuit/chatdiary/controller/diary/DiaryStreakController.java
+++ src/main/java/com/kuit/chatdiary/controller/diary/DiaryStreakController.java
@@ -1,7 +1,31 @@
 package com.kuit.chatdiary.controller.diary;
 
-import org.springframework.stereotype.Controller;
+import com.kuit.chatdiary.dto.diary.DiaryStreakResponseDTO;
+import com.kuit.chatdiary.service.diary.DiaryStreakService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("diary")
 public class DiaryStreakController {
+
+    private final DiaryStreakService diaryStreakService;
+
+    public DiaryStreakController(DiaryStreakService diaryStreakService) {
+        this.diaryStreakService = diaryStreakService;
+    }
+
+    @GetMapping("/streak")
+    public ResponseEntity<DiaryStreakResponseDTO> getStreakDate(
+            @RequestParam("memberId") Long userId,
+            @RequestParam("date") LocalDate today
+    ){
+        DiaryStreakResponseDTO response=diaryStreakService.streakDate(userId,today);
+        return ResponseEntity.ok(response);
+    }
 }
--- src/main/java/com/kuit/chatdiary/service/diary/DiaryStreakService.java
+++ src/main/java/com/kuit/chatdiary/service/diary/DiaryStreakService.java
@@ -1,4 +1,23 @@
 package com.kuit.chatdiary.service.diary;
 
+import com.kuit.chatdiary.dto.diary.DiaryStreakResponseDTO;
+import com.kuit.chatdiary.repository.diary.DiaryStreakRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
 public class DiaryStreakService {
+    public final DiaryStreakRepository diaryStreakRepository;
+
+    public DiaryStreakService(DiaryStreakRepository diaryStreakRepository) {
+        this.diaryStreakRepository = diaryStreakRepository;
+    }
+
+    public DiaryStreakResponseDTO streakDate(long userId, LocalDate today){
+        long streakDate=diaryStreakRepository.streakDate(userId,today);
+        DiaryStreakResponseDTO response = new DiaryStreakResponseDTO(streakDate);
+        return response;
+    }
+
 }
