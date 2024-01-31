cf843fb694af64386a8e6e5efa4f71cf364aa9fa
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-02-01 00:21:55 +0900

    Feat : 주석 추가
    작업 수정시 용이를 위해 아이디어 주석에 기록

--- src/main/java/com/kuit/chatdiary/repository/diary/DiaryStreakRepository.java
+++ src/main/java/com/kuit/chatdiary/repository/diary/DiaryStreakRepository.java
@@ -34,6 +34,14 @@ public class DiaryStreakRepository {
         long streakCount = 1;
         Date lastDate = diaries.get(0).getDiaryDate();
 
+        /** 구상도 (리펙시 주석 줄일 예정)
+         * 선택정렬 알고리즘 처럼
+         * 위에서 오늘이라는 검증 끝내면 lastDate라는 것은 리스트의 마지막 일기 일자
+         * 반복문에서 0이 아닌 i=1 부터 시작
+         * 즉 오늘 작성한 일기, 오늘 작성한 일기에서 하루 뺀 날짜와 currentDate의 날짜가 같다면 연속 일수 늘림
+         * currentDate 가 이제 lastDate가 되어 하루 전날짜와 그다음 i=2일때 일기의 일자가 같다면 streakCount 늘림
+         * */
+
         for(int i=1; i<diaries.size(); i++){
             Date currentDate = diaries.get(i).getDiaryDate();
             if(lastDate.toLocalDate().minusDays(1).equals(currentDate.toLocalDate())){
