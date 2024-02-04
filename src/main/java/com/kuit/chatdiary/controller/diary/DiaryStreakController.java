fb04fde2b3795a39827b87831d4e84501e23ede3
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-02-04 23:35:18 +0900

    Feat: CHAT-238-BE-API-스트릭-일기 (#34)
    * Feat : DiaryStreakRepository 작성
    
    DTO와 Repository 작성
    
    * Feat : 레포지토리 수정
    
    DTO엔 유저 아이디 제외, 레포지는 현재 날짜를 기준으로 연속기록 측정하도록 함
    
    * Feat : 서비스 계층및 컨트롤러 계층 작성
    
    레포지토리 사용
    
    * Feat : 주석 추가
    
    작업 수정시 용이를 위해 아이디어 주석에 기록
    
    * Feat : 서비스 계층 로직 수정
    
    리뷰 반영: 파라미터로 시간을 받는것이 아니라, 서버시간을 사용하도록 수정

---
+++ src/main/java/com/kuit/chatdiary/controller/diary/DiaryStreakController.java
@@ -0,0 +1,29 @@
+package com.kuit.chatdiary.controller.diary;
+
+import com.kuit.chatdiary.dto.diary.DiaryStreakResponseDTO;
+import com.kuit.chatdiary.service.diary.DiaryStreakService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("diary")
+public class DiaryStreakController {
+
+    private final DiaryStreakService diaryStreakService;
+
+    public DiaryStreakController(DiaryStreakService diaryStreakService) {
+        this.diaryStreakService = diaryStreakService;
+    }
+
+    @GetMapping("/streak")
+    public ResponseEntity<DiaryStreakResponseDTO> getStreakDate(
+            @RequestParam("memberId") Long userId){
+        DiaryStreakResponseDTO response=diaryStreakService.streakDate(userId);
+        return ResponseEntity.ok(response);
+    }
+}
---
+++ src/main/java/com/kuit/chatdiary/repository/diary/DiaryStreakRepository.java
@@ -0,0 +1,58 @@
+package com.kuit.chatdiary.repository.diary;
+
+import com.kuit.chatdiary.domain.Diary;
+import jakarta.persistence.EntityManager;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public class DiaryStreakRepository {
+
+    private final EntityManager em;
+
+    public DiaryStreakRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    public Long streakDate(long userId, LocalDate today){
+        String jpql = "SELECT d FROM diary d WHERE d.member.userId = :userId ORDER BY d.diaryDate DESC";
+
+        List<Diary> diaries =em.createQuery(jpql,Diary.class)
+                .setParameter("userId",userId)
+                .getResultList();
+
+        /** 오늘 날짜 기준으로 조회하기위한 조건문
+         *  연속 기록있어도 조회기준 날짜 즉 오늘 작성 일기 없다면 연속작성 기록 없는거로
+         * */
+        if (diaries.isEmpty() || !diaries.get(0).getDiaryDate().toLocalDate().equals(today)) {
+            return 0L;
+        }
+
+        long streakCount = 1;
+        Date lastDate = diaries.get(0).getDiaryDate();
+
+        /** 구상도 (리펙시 주석 줄일 예정)
+         * 선택정렬 알고리즘 처럼
+         * 위에서 오늘이라는 검증 끝내면 lastDate라는 것은 리스트의 마지막 일기 일자
+         * 반복문에서 0이 아닌 i=1 부터 시작
+         * 즉 오늘 작성한 일기, 오늘 작성한 일기에서 하루 뺀 날짜와 currentDate의 날짜가 같다면 연속 일수 늘림
+         * currentDate 가 이제 lastDate가 되어 하루 전날짜와 그다음 i=2일때 일기의 일자가 같다면 streakCount 늘림
+         * */
+
+        for(int i=1; i<diaries.size(); i++){
+            Date currentDate = diaries.get(i).getDiaryDate();
+            if(lastDate.toLocalDate().minusDays(1).equals(currentDate.toLocalDate())){
+                streakCount++;
+                lastDate = currentDate;
+            }else{
+                break;
+            }
+        }
+
+        return streakCount;
+    }
+
+}
