be4a89f80d5756db6f4daae45359549cb8e42fef
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-02-04 23:35:18 +0900

    Feat: CHAT-238-BE-API-스트릭-일기 (#34)
    * Feat : DiaryStreakRepository 작성
    
    DTO와 Repository 작성
    
    * Feat : 레포지토리 수정
    
    DTO엔 유저 아이디 제외, 레포지는 현재 날짜를 기준으로 연속기록 측정하도록 함
    
    * Feat : 서비스 계층및 컨트롤러 계층 작성
    
    레포지토리 사용
    
    * Feat : 주석 추가
    
    작업 수정시 용이를 위해 아이디어 주석에 기록
    
    * Feat : 서비스 계층 로직 수정
    
    리뷰 반영: 파라미터로 시간을 받는것이 아니라, 서버시간을 사용하도록 수정

---
+++ src/main/java/com/kuit/chatdiary/dto/diary/DiaryStreakResponseDTO.java
@@ -0,0 +1,12 @@
+package com.kuit.chatdiary.dto.diary;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class DiaryStreakResponseDTO {
+    private long diaryStreakDate;
+}
