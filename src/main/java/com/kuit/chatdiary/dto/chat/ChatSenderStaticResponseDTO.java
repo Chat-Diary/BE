7188bd0b7be9622237ee8b5370737d4b11371627
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-02-05 00:13:52 +0900

    Feat : DTO 수정
    리뷰 요구사항을 위해 DTO 수정

--- src/main/java/com/kuit/chatdiary/dto/chat/ChatSenderStaticResponseDTO.java
+++ src/main/java/com/kuit/chatdiary/dto/chat/ChatSenderStaticResponseDTO.java
@@ -7,15 +7,14 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.sql.Date;
+import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ChatSenderStaticResponseDTO {
-
-    private Sender sender;
-    private Long chatCount;
-    private double percentage;
     private Date startDate;
     private Date endDate;
+    private List<ChatSenderStaticResponseDTO> statistics;
 }
