0a789666d2b205c6590b6fd70ecd5e9f6adf97d2
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-02-05 04:52:34 +0900

    Feat: CHAT-BE-275-태그-통계-기능-수정 (#40)
    * Feat : 컨트롤러 계층 로직 수정
    
    리뷰 반영: 파라미터로 시간을 받는것이 아니라, 서버시간을 사용하도록 수정
    
    * Feat : 리펙토링
    
    컨트롤러 계층 코드 단순화, 서비스 계층에 기존 메서드가 혼자 너무 많은 책임을 지고있어 매서드 분할을 통해 책임 분배
    
    * Feat : DTO 추가 작성 + 수정
    
    응답형식 수정을 위한 DTO 구조 변경
    
    * Feat : DTO 수정에 따른 나머지 계층 수정
    
    TagStatisticsWithDateDTO 라는 DTO로 반환 정보를 반환하도록 수정
    
    * Feat : 네이밍컨벤션을 위해 수정
    
    TagStatisticsWithDateResponseDTO 로 클래스 이름 수정
    
    * Refactor: DateRange 구하는 로직을 대화 상대 통계와 동일하게 변경
    
    ---------
    
    Co-authored-by: nonaninona <starcraft0529@gmail.com>

--- src/main/java/com/kuit/chatdiary/controller/diary/DiaryTagStatisticsController.java
+++ src/main/java/com/kuit/chatdiary/controller/diary/DiaryTagStatisticsController.java
@@ -1,14 +1,9 @@
 package com.kuit.chatdiary.controller.diary;
 
-import com.kuit.chatdiary.dto.diary.DateRangeDTO;
-import com.kuit.chatdiary.dto.diary.TagStatisticResponseDTO;
+import com.kuit.chatdiary.dto.diary.TagStatisticsWithDateResponseDTO;
 import com.kuit.chatdiary.service.diary.DiaryTagStatisticsService;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDate;
-import java.util.List;
 
 @RestController
 @RequestMapping("/diary")
@@ -21,13 +16,10 @@ public class DiaryTagStatisticsController {
     }
 
     @GetMapping("/tags")
-    public ResponseEntity<List<TagStatisticResponseDTO>> getTagStatistics(
+    public ResponseEntity<TagStatisticsWithDateResponseDTO> getTagStatistics(
             @RequestParam("memberId") Long memberId,
-            @RequestParam("type") String type,
-            @RequestParam("date") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate localDate) {
-        DateRangeDTO dateRange = diaryTagStatisticsService.staticsType(type,localDate);
-        List<TagStatisticResponseDTO> tagStatistics = diaryTagStatisticsService.calculateTagStatistics(memberId, dateRange.getStartDate()
-                ,dateRange.getEndDate());
+            @RequestParam("type") String type) {
+        TagStatisticsWithDateResponseDTO tagStatistics = diaryTagStatisticsService.calculateTagStatistics(memberId, type);
         return ResponseEntity.ok(tagStatistics);
     }
 }
