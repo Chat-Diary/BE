8a6bdc4e0d2f1e0ebdca8b51981f565ba609050c
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-02-05 04:21:13 +0900

    Feat: CHAT-260-BE-API-대화-상대-통계-수정 (#38)
    * Feat : 서비스 계층 로직 수정
    
    기존에 복잡한 로직 맵을 적극 사용해서 수정 및, 응답에서 USER 제외
    
    * Feat : 서비스 계층 로직 수정
    
    리뷰 반영: 파라미터로 시간을 받는것이 아니라, 서버시간을 사용하도록 수정
    
    * Feat : DTO 수정
    
    리뷰 요구사항을 위해 DTO 수정
    
    * Feat : 응답 구조 수정
    
    상위 DTO 작성을 통해 응답구조 변경, 날짜와 각 보낸이에 대한 정보, 그에따른 컨트롤러 자료형 수정
    
    * Feat : 주석
    
    * Feat: 백엔드 API 서버에 대한 https 적용 (#39)
    
    Chore : docker hub 경로 숨김
    
    * Fix : develop에 PR시 CI/CD 동작 제거
    
    * Feat : 서비스 계층 수정
    
    USER totalChats 계산에서 제외
    
    ---------
    
    Co-authored-by: nonaninona <53222283+nonaninona@users.noreply.github.com>
    Co-authored-by: nonaninona <starcraft0529@gmail.com>

--- src/main/java/com/kuit/chatdiary/controller/SenderController.java
+++ src/main/java/com/kuit/chatdiary/controller/SenderController.java
@@ -1,17 +1,13 @@
 package com.kuit.chatdiary.controller;
 
-import com.kuit.chatdiary.dto.chat.ChatSenderStaticResponseDTO;
+import com.kuit.chatdiary.dto.chat.ChatSenderStatisticsResponseDTO;
 import com.kuit.chatdiary.dto.diary.DateRangeDTO;
 import com.kuit.chatdiary.service.chat.SenderService;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.util.List;
 
 @RestController
 @RequestMapping("/chat")
@@ -24,14 +20,12 @@ public class SenderController {
     }
 
     @GetMapping("/sender")
-    public ResponseEntity<List<ChatSenderStaticResponseDTO>> getTagStatistics(
+    public ResponseEntity<ChatSenderStatisticsResponseDTO> getSenderStatistics(
             @RequestParam("memberId") Long memberId,
-            @RequestParam("type") String type,
-            @RequestParam("date") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate localDate) {
-        DateRangeDTO dateRange = senderService.staticsType(type,localDate);
-        List<ChatSenderStaticResponseDTO> tagStatistics = senderService.calculateSenderStatistics(memberId, dateRange.getStartDate()
-                ,dateRange.getEndDate());
-        return ResponseEntity.ok(tagStatistics);
+            @RequestParam("type") String type) {
+        DateRangeDTO dateRange = senderService.staticsType(type);
+        ChatSenderStatisticsResponseDTO senderStatistics = senderService.calculateSenderStatistics(memberId, dateRange.getStartDate(), dateRange.getEndDate());
+        return ResponseEntity.ok(senderStatistics);
     }
 
 }
--- src/main/java/com/kuit/chatdiary/dto/chat/ChatSenderStatisticsResponseDTO.java
+++ src/main/java/com/kuit/chatdiary/dto/chat/ChatSenderStatisticsResponseDTO.java
@@ -1,21 +1,19 @@
 package com.kuit.chatdiary.dto.chat;
 
-import com.kuit.chatdiary.domain.Sender;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.sql.Date;
+import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class ChatSenderStaticResponseDTO {
-
-    private Sender sender;
-    private Long chatCount;
-    private double percentage;
+public class ChatSenderStatisticsResponseDTO {
     private Date startDate;
     private Date endDate;
+    private List<ChatSenderDetailResponseDTO> statistics;
 }
