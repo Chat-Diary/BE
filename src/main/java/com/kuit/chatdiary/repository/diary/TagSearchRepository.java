886a1b63dbc978083d178ac1deb3d123a510df89
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-02-06 11:55:06 +0900

    Fix : 커밋 취소

--- src/main/java/com/kuit/chatdiary/repository/diary/TagSearchRepository.java
+++ src/main/java/com/kuit/chatdiary/repository/diary/TagSearchRepository.java
@@ -1,6 +1,7 @@
 package com.kuit.chatdiary.repository.diary;
 
 import com.kuit.chatdiary.domain.Diary;
+import com.kuit.chatdiary.dto.diary.TagInfoDTO;
 import com.kuit.chatdiary.dto.diary.TagSearchResponseDTO;
 import jakarta.persistence.EntityManager;
 import org.springframework.stereotype.Repository;
@@ -36,23 +37,19 @@ public class TagSearchRepository {
             TagSearchResponseDTO response = new TagSearchResponseDTO();
             response.setDiaryId(diary.getDiaryId());
             response.setTitle(diary.getTitle());
-            response.setContent(diary.getContent());
             response.setDiaryDate(diary.getDiaryDate());
-            response.setCreateAt(diary.getCreateAt());
-            response.setUpdateAt(diary.getUpdateAt());
-            response.setStatus(diary.getStatus());
-
             List<String> photoUrls = em.createQuery(
                             "SELECT dp.photo.imageUrl FROM diaryphoto dp WHERE dp.diary.diaryId = :diaryId", String.class)
                     .setParameter("diaryId", diary.getDiaryId())
                     .getResultList();
             response.setPhotoUrls(photoUrls);
 
-            List<String> tagNames = em.createQuery(
-                            "SELECT t.tagName FROM diarytag dt JOIN dt.tag t WHERE dt.diary.diaryId = :diaryId", String.class)
+            List<TagInfoDTO> tagList = em.createQuery(
+                            "SELECT new com.kuit.chatdiary.dto.diary.TagInfoDTO(t.tagId, t.tagName) " +
+                                    "FROM diarytag dt JOIN dt.tag t WHERE dt.diary.diaryId = :diaryId", TagInfoDTO.class)
                     .setParameter("diaryId", diary.getDiaryId())
                     .getResultList();
-            response.setTagList(tagNames);
+            response.setTagList(tagList);
 
             return response;
         }).collect(Collectors.toList());
