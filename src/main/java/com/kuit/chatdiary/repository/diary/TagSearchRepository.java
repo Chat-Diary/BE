ddde38c8e6d93f6ee60ab4d05e0906479ac62ddd
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-02-06 11:31:43 +0900

    Feat : 레포지토리 수정사항 반영
    레포지토리 일반적으로 수정, 쿼리문이 복잡해서 맞는지 모르겠음..

--- src/main/java/com/kuit/chatdiary/repository/diary/TagSearchRepository.java
+++ src/main/java/com/kuit/chatdiary/repository/diary/TagSearchRepository.java
@@ -6,6 +6,8 @@ import com.kuit.chatdiary.dto.diary.TagSearchResponseDTO;
 import jakarta.persistence.EntityManager;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,43 +19,60 @@ public class TagSearchRepository {
         this.em = em;
     }
 
-
-    /** 지식 한계로 쿼리문 세개를 나눠서.. */
+    /** 레포지에서 DTO 매핑까지 다 하기 */
     public List<TagSearchResponseDTO> findByTag(List<String> tagName, Long userId) {
-        int tagCount = tagName.size();
-        List<Diary> diaries = em.createQuery(
-                        "SELECT d FROM diary d " +
-                                "JOIN diarytag dt ON d.diaryId = dt.diary.diaryId " +
-                                "JOIN tag t ON dt.tag.tagId = t.tagId " +
-                                "WHERE t.tagName IN :tagNames AND d.member.userId = :userId " +
-                                "GROUP BY d " +
-                                "HAVING COUNT(DISTINCT t) = :tagCount", Diary.class)
+        String diarySql = "SELECT d.diary_id, d.title, d.diary_date " +
+                "FROM diary d " +
+                "INNER JOIN diarytag dt ON d.diary_id = dt.diary_id " +
+                "INNER JOIN tag t ON dt.tag_id = t.tag_id " +
+                "WHERE t.tag_name IN :tagNames AND d.user_id = :userId " +
+                "GROUP BY d.diary_id, d.title, d.diary_date " +
+                "HAVING COUNT(DISTINCT t.tag_id) = :tagCount";
+
+        List<Object[]> diaryResults = em.createNativeQuery(diarySql)
                 .setParameter("tagNames", tagName)
                 .setParameter("userId", userId)
-                .setParameter("tagCount", (long) tagCount)
+                .setParameter("tagCount", Long.valueOf(tagName.size()))
                 .getResultList();
 
-        return diaries.stream().map(diary -> {
-            TagSearchResponseDTO response = new TagSearchResponseDTO();
-            response.setDiaryId(diary.getDiaryId());
-            response.setTitle(diary.getTitle());
-            response.setDiaryDate(diary.getDiaryDate());
-            List<String> photoUrls = em.createQuery(
-                            "SELECT dp.photo.imageUrl FROM diaryphoto dp WHERE dp.diary.diaryId = :diaryId", String.class)
-                    .setParameter("diaryId", diary.getDiaryId())
-                    .getResultList();
-            response.setPhotoUrls(photoUrls);
+        List<TagSearchResponseDTO> responses = new ArrayList<>();
+        for (Object[] diaryResult : diaryResults) {
+            Long diaryId = ((Number) diaryResult[0]).longValue();
+            String title = (String) diaryResult[1];
+            Date diaryDate = (Date) diaryResult[2];
 
-            List<TagInfoDTO> tagList = em.createQuery(
-                            "SELECT new com.kuit.chatdiary.dto.diary.TagInfoDTO(t.tagId, t.tagName) " +
-                                    "FROM diarytag dt JOIN dt.tag t WHERE dt.diary.diaryId = :diaryId", TagInfoDTO.class)
-                    .setParameter("diaryId", diary.getDiaryId())
+            String photoSql = "SELECT p.image_url " +
+                    "FROM diaryphoto dp " +
+                    "INNER JOIN photo p ON dp.photo_id = p.photo_id " +
+                    "WHERE dp.diary_id = :diaryId";
+            List<String> photoUrls = em.createNativeQuery(photoSql)
+                    .setParameter("diaryId", diaryId)
                     .getResultList();
+
+            String tagSql = "SELECT t.tag_id, t.tag_name " +
+                    "FROM diarytag dt " +
+                    "INNER JOIN tag t ON dt.tag_id = t.tag_id " +
+                    "WHERE dt.diary_id = :diaryId";
+            List<Object[]> tagResults = em.createNativeQuery(tagSql)
+                    .setParameter("diaryId", diaryId)
+                    .getResultList();
+
+            List<TagInfoDTO> tagList = tagResults.stream()
+                    .map(tagResult -> new TagInfoDTO(
+                            ((Number) tagResult[0]).longValue(),
+                            (String) tagResult[1]))
+                    .collect(Collectors.toList());
+
+            TagSearchResponseDTO response = new TagSearchResponseDTO();
+            response.setDiaryId(diaryId);
+            response.setTitle(title);
+            response.setDiaryDate(diaryDate);
+            response.setPhotoUrls(photoUrls);
             response.setTagList(tagList);
 
-            return response;
-        }).collect(Collectors.toList());
+            responses.add(response);
+        }
+
+        return responses;
     }
-
-
 }
