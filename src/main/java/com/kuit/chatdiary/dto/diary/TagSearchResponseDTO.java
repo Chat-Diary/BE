f1997e534e7fb1f015604959e91cc0af8c9c0cf5
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-02-06 11:40:35 +0900

    Revert "Feat : 수정사항 반영"
    This reverts commit 9153cf67e91afb009c17504bab89866da0f2cf1e.
    
    # Conflicts:
    #	src/main/java/com/kuit/chatdiary/repository/diary/TagSearchRepository.java

--- src/main/java/com/kuit/chatdiary/dto/diary/TagSearchResponseDTO.java
+++ src/main/java/com/kuit/chatdiary/dto/diary/TagSearchResponseDTO.java
@@ -1,17 +1,12 @@
 package com.kuit.chatdiary.dto.diary;
 
-import com.kuit.chatdiary.domain.Diary;
-import com.kuit.chatdiary.domain.DiaryPhoto;
-import com.kuit.chatdiary.domain.DiaryTag;
 import com.kuit.chatdiary.domain.Photo;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import java.time.LocalDateTime;
-import java.util.Collections;
 import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -20,21 +15,11 @@ public class TagSearchResponseDTO {
 
     private Long diaryId;
     private String title;
+    private String content;
     private Date diaryDate;
+    private LocalDateTime createAt;
+    private LocalDateTime updateAt;
+    private String status;
+    private List<String> tagList;
     private List<String> photoUrls;
-    private List<TagInfoDTO> tagList;
-
-    public TagSearchResponseDTO(Diary diary, List<DiaryPhoto> diaryPhotos, List<DiaryTag> diaryTags) {
-        this.diaryId = diary.getDiaryId();
-        this.title = diary.getTitle();
-        this.diaryDate = diary.getDiaryDate();
-        this.photoUrls = diaryPhotos != null ? diaryPhotos.stream()
-                .map(DiaryPhoto::getPhoto)
-                .map(Photo::getImageUrl)
-                .collect(Collectors.toList()) : Collections.emptyList();
-        this.tagList = diaryTags != null ? diaryTags.stream()
-                .map(diaryTag -> new TagInfoDTO(diaryTag.getTag().getTagId(), diaryTag.getTag().getTagName()))
-                .collect(Collectors.toList()) : Collections.emptyList();
-    }
-
 }
