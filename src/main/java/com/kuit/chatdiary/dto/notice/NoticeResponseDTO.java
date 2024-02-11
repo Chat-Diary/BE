7aa5b469779b526f981fe74bae556aeec990b575
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-02-11 23:41:37 +0900

    Fix : 주석

--- src/main/java/com/kuit/chatdiary/dto/notice/NoticeResponseDTO.java
+++ src/main/java/com/kuit/chatdiary/dto/notice/NoticeResponseDTO.java
@@ -16,6 +16,8 @@ public class NoticeResponseDTO {
     private String title;
     private String content;
 
+
+    /** 지금은 쿼리로 날려서 수작업으로 날짜 입력하지만 나중에 작성 매서드 만들면 .now 등으로 서버 날짜 쓰기*/
     private Date noticeDate;
 
 }
