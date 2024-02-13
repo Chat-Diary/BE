1750b47cd220d809306cf803390bd2ec84475891
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-02-13 20:28:52 +0900

    Feat : DTO 변경

--- src/main/java/com/kuit/chatdiary/dto/CalendarInquiryResponseDTO.java
+++ src/main/java/com/kuit/chatdiary/dto/CalendarInquiryResponseDTO.java
@@ -1,18 +1,17 @@
 package com.kuit.chatdiary.dto;
 
+import com.kuit.chatdiary.domain.Sender;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDate;
-import java.util.List;
-
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class DateInquiryResponse {
-    private List<LocalDate> dates;
-    private List<CalendarInquiryResponse> responses;
+public class CalendarInquiryResponseDTO {
+    private String dates;
+    private Sender sender;
+    private boolean exists;
 }
