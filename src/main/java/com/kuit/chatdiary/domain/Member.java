55f454befe5a527580f9a6a5131753c79de159fc
dainshon <81453127+dainshon@users.noreply.github.com>
2024-02-17 01:28:30 +0900

    Feat: CHAT-225-카카오로그인-userId도반환 (#57)
    userId, nickname 함께 반환

--- src/main/java/com/kuit/chatdiary/domain/Member.java
+++ src/main/java/com/kuit/chatdiary/domain/Member.java
@@ -5,6 +5,7 @@ import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -42,4 +43,7 @@ public class Member {
     @ColumnDefault("'ACTIVE'")
     private String status;
 
+
 }
+
+
--- src/main/java/com/kuit/chatdiary/repository/MemberRepository.java
+++ src/main/java/com/kuit/chatdiary/repository/MemberRepository.java
@@ -3,6 +3,8 @@ package com.kuit.chatdiary.repository;
 import com.kuit.chatdiary.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Member findByNickname(String nickname);
+    Optional<Member> findByEmail(String email);
 }
