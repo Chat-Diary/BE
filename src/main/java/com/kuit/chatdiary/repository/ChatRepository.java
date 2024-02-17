d43525479f038bfcd5bd11729764a58488b8eef9
nonaninona <starcraft0529@gmail.com>
2024-02-17 22:12:49 +0900

    Fix: 채팅 개수 10개로 조정

--- src/main/java/com/kuit/chatdiary/repository/ChatRepository.java
+++ src/main/java/com/kuit/chatdiary/repository/ChatRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 public interface ChatRepository extends JpaRepository<Chat, Long> {
 
     List<Chat> findTop10ByMember_UserIdOrderByChatIdDesc(Long userId);
-    @Query("SELECT c FROM chat c WHERE c.member.userId = :userId AND :lastChatId-10 < c.chatId AND c.chatId < :lastChatId ORDER BY c.chatId ASC")
+    @Query("SELECT c FROM chat c WHERE c.member.userId = :userId AND :lastChatId-10 < c.chatId AND c.chatId <= :lastChatId ORDER BY c.chatId ASC")
     List<Chat> findTop10ByUserIdAndChatIdLessThanOrderByChatIdDesc(@Param("userId") Long userId, @Param("lastChatId") Long lastChatId);
     List<Chat> findTopByMember_UserIdOrderByChatIdDesc(Long userId);
 }
